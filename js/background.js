64aef88bb09d35a273485d5c83b454c274d5a91d
Taekyung An <atk0426@naver.com>
2022-07-10 21:24:13 +0900

    random background 구현

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = 
+["https://raw.githubusercontent.com/nomadcoders/javascript-for-beginners/5874df713ba5476174c5df78dc886198c38ef8dc/img/0.jpeg", 
+"https://raw.githubusercontent.com/nomadcoders/javascript-for-beginners/5874df713ba5476174c5df78dc886198c38ef8dc/img/1.jpeg", 
+"https://raw.githubusercontent.com/nomadcoders/javascript-for-beginners/5874df713ba5476174c5df78dc886198c38ef8dc/img/2.jpeg"]
+
+const chosenImage = images [Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src= `${chosenImage}`
+
+console.log(bgImage);
+
+document.body.appendChild(bgImage);
